46bc302d52f718123443e3d93f487ce074201b73
Dario <dariopirrello.job@gmail.com>
2020-04-13 19:13:20 +0200

    [RandomClassification] Create a random classfication Project

---
+++ RandomClassification/src/labda/LabdaExpression.java
@@ -0,0 +1,12 @@
+package labda;
+
+public class LabdaExpression {
+	
+	public static void main(String[] args) 
+	{ 
+		 Menbers cmp = new Menbers("Vita", "Rita", "Daniele", "Dario", "Claudio");
+		 cmp.start();
+	}
+	
+}
+	
